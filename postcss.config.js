8653b38b9124d49452ae8efc2119967c88565b63
angeliaz <angeliaw@yeah.net>
2019-02-14 16:35:59 +0800

    增加postcss px to vw插件配置

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: {
+    'postcss-px-to-viewport': {
+      viewportWidth: 750, viewportHeight: 1334, unitPrecision: 5, viewportUnit: 'vw', selectorBlackList: [], minPixelValue: 1, mediaQuery: false
+    }
+  }
+};
---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+import promise from './promise/promise.js';
+import '../css/main.less';
+
+// addImage.js
+/* let smallImg = document.createElement('img')
+// 必须 require 进来
+smallImg.src = require('../images/newer.png')
+// console.log(smallImg)
+document.body.appendChild(smallImg) */
+
+/* let bigImg = document.createElement('img')
+bigImg.src = require('../images/big.jpeg')
+document.body.appendChild(bigImg) */
+
+let a = 23333;
+
+function user(params) {
+  console.log('user');
+}
+
+// promise.dbFuc();
+
+// 按需加载
+document.getElementById('btn').onclick = () => {
+  import(/* webpackChunkName: 'dynamic1' */'./dynamic.js')
+  .then(module => {
+  console.log(module.test('dynamic11111'))
+  })
+}
+
+console.log('index1111');
